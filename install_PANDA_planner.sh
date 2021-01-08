058d92a7db8a6f2a03ff06d5adaabe0ae7b55481
Lou <lou.lauter@t-online.de>
2021-01-08 13:59:38 +0100

    added Panda planner installer (downloader)

---
+++ install_PANDA_planner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit immediately if any command exits with a nonzero exit value
+set -e
+
+# Download planner
+PANDA_DOWNLOAD_URL="https://www.uni-ulm.de/fileadmin/website_uni_ulm/iui.inst.090/panda/PANDA.jar"
+
+# Install paths
+INSTALL_DIR_ROOT="/opt/ropod/task-planner/bin"
+INSTALL_DIR_NAME="panda-planner"
+INSTALL_DIR=$INSTALL_DIR_ROOT/$INSTALL_DIR_NAME
+
+# create directory (if not already exists)
+mkdir -p $INSTALL_DIR
+
+# Download Planner (replace existing file)
+echo "Installing PANDA planner"
+cd $INSTALL_DIR
+wget -O PANDA.jar $PANDA_DOWNLOAD_URL
